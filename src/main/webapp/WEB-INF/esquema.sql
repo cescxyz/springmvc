0e14c8800e8319df86359ded11972df05048df9e
cesc <cesc@mail.com>
2018-12-12 00:48:07 -0600

    Integrando Spring Security

--- src/main/webapp/WEB-INF/esquema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists persona;
-
-create table persona (  
-    id_persona identity primary key,  
-    nombre varchar(50) not null,  
-    ape_paterno varchar(50) not null,  
-    ape_materno varchar(50),  
-    email varchar(50) not null
-);
